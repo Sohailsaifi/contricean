abd94cbe1b0090f09144f445eab7c4d65852d262
Sohailsaifi <sohailsaifi65@gmail.com>
2024-01-11 22:09:33 +0530

    Production config import removed

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 # build_files.sh
 pip install -r requirements.txt
+sudo apt-get install libsqlite3-dev libsqlite3
+pip install pysqlite
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic
